7e8800a8c5ddb4c44b147078b13a18c883062d9f
KafetzisThomas <tomkafetzis06@gmail.com>
2026-04-15 15:01:15 +0300

    Rename file url endpoints

--- files/urls.py
+++ files/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "files"
 urlpatterns = [
     path("", views.drive, name="drive"),
-    path("new_file/", views.new_file, name="new_file"),
-    path("delete_file/<int:pk>", views.delete_file, name="delete_file"),
+    path("upload/", views.new_file, name="new_file"),
+    path("delete/<int:pk>", views.delete_file, name="delete_file"),
 ]
