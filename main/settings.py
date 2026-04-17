6330e5444811868933bbb06d1bb4e17c5b04f7d4
KafetzisThomas <tomkafetzis06@gmail.com>
2026-04-17 13:03:33 +0300

    Update base and drive templates layout

--- main/settings.py
+++ main/settings.py
@@ -137,7 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 LOGIN_URL = "users:login"
 LOGIN_REDIRECT_URL = "files:drive"
