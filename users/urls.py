b227e94b66cd75a1360709bd42968f621deb5045
KafetzisThomas <tomkafetzis06@gmail.com>
2026-04-18 15:09:38 +0300

    Add user account view

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import RegisterView
+from .views import RegisterView, AccountView
 
 app_name = "users"
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("account/", AccountView.as_view(template_name="users/account.html"), name="account"),
     path("logout/", auth_views.LogoutView.as_view(next_page="users:login"), name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,22 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import UserUpdateForm
 
 
 class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = "users/register.html"
     success_url = reverse_lazy("users:login")
+
+
+class AccountView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    template_name = "users/account.html"
+    success_url = reverse_lazy("files:drive")
+
+    def get_object(self):
+        return self.request.user
