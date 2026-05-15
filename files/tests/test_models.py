f4357be324ce090a706a2a52933c4f595ff25c48
KafetzisThomas <tomkafetzis06@gmail.com>
2026-05-15 21:47:34 +0300

    Add tests for Folder model

--- files/tests/test_models.py
+++ files/tests/test_models.py
@@ -2,7 +2,7 @@ import uuid
 from django.test import TestCase
 from unittest.mock import Mock
 from django.contrib.auth import get_user_model
-from ..models import user_file_path
+from ..models import user_file_path, Folder
 
 User = get_user_model()
 
@@ -29,3 +29,22 @@ class UserFilePathTests(TestCase):
     def test_file_with_multiple_dots(self):
         path = user_file_path(self.mock_instance, 'archive.tar.gz')
         self.assertTrue(path.endswith('.gz'))
+
+
+class FolderModelTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser')
+
+    def test_folder_creation(self):
+        folder = Folder.objects.create(name="Documents", owner=self.user)
+        self.assertEqual(str(folder), "Documents")
+        self.assertEqual(folder.owner, self.user)
+        self.assertIsNone(folder.parent)
+
+    def test_folder_nesting(self):
+        root = Folder.objects.create(name="Root", owner=self.user)
+        child = Folder.objects.create(name="Child", parent=root, owner=self.user)
+        self.assertEqual(root.subfolders.count(), 1)
+        self.assertEqual(root.subfolders.first(), child)
+        self.assertEqual(child.parent, root)
