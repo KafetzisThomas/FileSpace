59d80b5642d8f43ee5f917f33d5dcadafa310429
KafetzisThomas <tomkafetzis06@gmail.com>
2026-06-01 20:14:01 +0300

    Add tests for PasswordChangeForm

--- users/tests/test_forms.py
+++ users/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from ..forms import RegistrationForm
+from ..forms import RegistrationForm, PasswordChangeForm
 
 User = get_user_model()
 
@@ -25,4 +25,28 @@ class RegistrationFormTests(TestCase):
 
     def test_password_strength_validation_strong(self):
         form = RegistrationForm(data=self.password_strength_strong)
+        self.assertTrue(form.is_valid(), form.errors)
+
+
+class PasswordChangeFormTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="user", password="Str0ng_p@ssword")
+        self.password_weak = {
+            "old_password": "Str0ng_p@ssword",
+            "new_password1": "password123",
+            "new_password2": "password123"
+        }
+        self.password_strong = {
+            "old_password": "Str0ng_p@ssword",
+            "new_password1": "New_Str0ng_p@ssword",
+            "new_password2": "New_Str0ng_p@ssword"
+        }
+
+    def test_password_change_weak(self):
+        form = PasswordChangeForm(user=self.user, data=self.password_weak)
+        self.assertFalse(form.is_valid(), form.errors)
+
+    def test_password_change_strong(self):
+        form = PasswordChangeForm(user=self.user, data=self.password_strong)
         self.assertTrue(form.is_valid(), form.errors)
