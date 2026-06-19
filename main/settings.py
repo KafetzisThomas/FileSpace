3f32ecf7d18bb19599d14f90847973e59525ab0c
KafetzisThomas <tomkafetzis06@gmail.com>
2026-06-19 19:06:47 +0300

    Use whitenoise for static files instead of S3

--- main/settings.py
+++ main/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -150,14 +151,17 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 if not DEBUG:
-    # prod: use S3 for media and static files
+    # prod: use S3 for media files
     STORAGES = {
         "default": {
             "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
         },
         "staticfiles": {
-            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
         },
     }
     AWS_ACCESS_KEY_ID = os.getenv("STORAGE_ACCESS_KEY")
@@ -167,10 +171,6 @@ if not DEBUG:
     AWS_S3_REGION_NAME = os.getenv("STORAGE_REGION_NAME")
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
-else:
-    # local: serve media files locally
-    MEDIA_URL = "/media/"
-    MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 SESSION_SAVE_EVERY_REQUEST = True
