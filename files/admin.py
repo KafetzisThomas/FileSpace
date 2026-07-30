5af40e4aff779d325d8fd1db1c58af356c54041f
KafetzisThomas <tomkafetzis06@gmail.com>
2026-07-30 09:12:49 +0300

    Display verbose admin list for folders and files

--- files/admin.py
+++ files/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Folder, File
 
-admin.site.register(Folder)
-admin.site.register(File)
+@admin.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "parent", "created_at")
+    list_filter = ("owner", "created_at")
+    search_fields = ("name", "owner__username")
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "folder", "size", "uploaded_at")
+    list_filter = ("owner", "uploaded_at")
+    search_fields = ("name", "owner__username")
